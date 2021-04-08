8df25709d867002899c2722f7309fa9a2f5e61d9
Nguyen Duc Long <nguyenduclong.ict@gmail.com>
2021-04-08 21:48:59 +0700

    init project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4eff4ea814ae660911cc465d95b48254'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '21ca7e02-a79e-4c9a-86da-206f640ba264'
+};
